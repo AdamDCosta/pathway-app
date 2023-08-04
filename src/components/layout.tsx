b60b9b4114fbb988d028cdb6df8de3cea7a00a03
AdamDCosta <adam.dcosta@googlemail.com>
2023-08-04 15:25:34 +0100

    basic home layout added

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,19 +1,22 @@
 import type { ReactNode } from "react";
 import { SignOutButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <>
-      <nav>todo...</nav>
-      <header>
-        <h1 className="text-foreground">Pathway</h1>
-      </header>
-      <main className="flex min-h-screen flex-col items-center justify-center">
-        {children}
-        <div>
-          <SignOutButton />
+    <div className="w-100 flex min-h-screen">
+      <nav className="border-foreground w-48 border">Sidenav...</nav>
+      <main className="flex flex-grow flex-col justify-start">
+        <header className="border-foreground w-100 border">
+          <h1 className="text-foreground">Header...</h1>
+        </header>
+        <div className="flex flex-grow flex-col items-center justify-center">
+          {children}
+          <SignOutButton>
+            <Button variant="default">Sign out</Button>
+          </SignOutButton>
         </div>
       </main>
-    </>
+    </div>
   );
 }
